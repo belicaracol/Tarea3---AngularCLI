c77833b10425076be3a352cb144675912b99fa33
belicaracol <58761120+belicaracol@users.noreply.github.com>
2021-10-28 00:21:52 -0300

    Add files via upload

---
+++ src/app/componentes/ver-noticias/ver-noticias.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {ListaNoticias,Noticias,ListaDestacadas} from '../../interfaces/noticias';
+import {ActivatedRoute,Route} from '@angular/router';
+import {Categorias,ListaCategorias} from '../../interfaces/categorias';
+@Component({
+  selector: 'app-ver-noticias',
+  templateUrl: './ver-noticias.component.html',
+  styleUrls: ['./ver-noticias.component.scss']
+})
+export class VerNoticiasComponent implements OnInit {
+
+  ListaNoticias=ListaNoticias;
+  ListaDestacadas=ListaDestacadas;
+  ListaCategorias=ListaCategorias;
+  id:number=0;
+  Noticia:any;
+  Categoria:any;
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+    for(this.id=1; this.id<=150; this.id++) {
+      if(this.id >= 100)
+      {
+        this.Noticia=ListaDestacadas.find(objeto=>objeto.id==this.id);
+        this.Categoria=ListaCategorias.find(objeto=>objeto.id==this.Noticia.idCategoria);
+      }
+      else
+      {
+        this.Noticia=ListaNoticias.find(objeto=>objeto.id==this.id);
+        this.Categoria=ListaCategorias.find(objeto=>objeto.id==this.Noticia.idCategoria);
+      }
+    }
+  }
+}
